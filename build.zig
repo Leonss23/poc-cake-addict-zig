589ae0c5ed6a8867822df083f376f74b7692042d
Leonardo Gatti <leonardogattidev@gmail.com>
2023-12-19 06:21:27 -0300

    renamed executable

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "cake-eater",
+        .name = "cake-addict",
         .root_source_file = .{ .path = "src/main.zig" },
         .target = target,
         .optimize = optimize,
